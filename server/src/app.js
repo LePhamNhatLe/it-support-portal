b0f96d19aee15bac3b4dd6a4dc940194aa7840d4
Lê Phạm Nhật Lễ <nhatle.app@gmail.com>
2026-09-07 11:55:42 +0700

    P21: create Express application

---
+++ server/src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const crypto = require("node:crypto");
+const env = require("./config/env");
+const apiRoutes = require("./routes");
+const { fail } = require("./utils/http");
+
+function createApp() {
+  const app = express();
+
+  app.disable("x-powered-by");
+  app.use(cors({ origin: env.CORS_ORIGIN, credentials: false }));
+  app.use(express.json({ limit: "1mb" }));
+
+  app.use((req, res, next) => {
+    req.requestId = crypto.randomUUID();
+    res.setHeader("X-Request-Id", req.requestId);
+    next();
+  });
+
+  app.use(env.API_PREFIX, apiRoutes);
+
+  app.use((req, res) => fail(res, 404, "ROUTE_NOT_FOUND", "API endpoint không tồn tại."));
+
+  app.use((error, req, res, next) => {
+    if (res.headersSent) return next(error);
+    console.error(`[${req.requestId || "no-request-id"}]`, error);
+    return fail(res, 500, "INTERNAL_ERROR", "Đã xảy ra lỗi phía máy chủ.");
+  });
+
+  return app;
+}
+
+module.exports = { createApp };
